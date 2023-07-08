04c8895b27392371fbff34e10bada6a8b7e7fa76
Isslea <91389716+Isslea@users.noreply.github.com>
2023-07-08 02:04:52 +0200

    fix bug with hide and show done tasks

--- lib/widgets/item_list.dart
+++ lib/widgets/item_list.dart
@@ -143,6 +143,8 @@ class _ItemListState extends State<ItemList> {
                 ),
               ),
             );
+          } else{
+            return Container();
           }
         },
       );
